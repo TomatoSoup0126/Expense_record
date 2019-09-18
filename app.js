63b17bd1809c8729b62bc88b65c2689a28f102f3
jonathantang <jonathantang0126@gmail.com>
2019-09-18 08:29:38 +0800

    Project start

---
+++ app.js
@@ -0,0 +1,19 @@
+//基本設定用常數
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+const port = 3000
+
+//定義模板引擎
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+//設定根目錄路由
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+//設定伺服器啟動
+app.listen(port, () => {
+  console.log(`Express app listening on port ${port}.`)
+})
