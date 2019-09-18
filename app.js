443df12831f3094619ad4e698e4283bc1d509581
jonathantang <jonathantang0126@gmail.com>
2019-09-18 08:45:06 +0800

    connect mongoDB

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 //基本設定用常數
 const express = require('express')
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
+
+mongoose.connect('mongodb://localhost/expenseRecord', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 
 //定義模板引擎
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
