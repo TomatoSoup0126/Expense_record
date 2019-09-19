7a8ae57cb7fc4f2c7a9d1b89c66f351634871be5
jonathantang <jonathantang0126@gmail.com>
2019-09-20 01:02:42 +0800

    set login check route

--- routes/user.js
+++ routes/user.js
@@ -11,8 +11,11 @@ router.get('/login', (req, res) => {
 })
 
 // 登入檢查
-router.post('/login', (req, res) => {
-  res.send('login')
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/users/login'
+  })(req, res, next)
 })
 
 // 註冊頁面
@@ -50,7 +53,8 @@ router.post('/register', (req, res) => {
 
 // 登出
 router.get('/logout', (req, res) => {
-  res.send('logout')
+  req.logout()
+  res.redirect('/users/login')
 })
 
 module.exports = router
