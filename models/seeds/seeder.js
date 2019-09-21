419c848c4d98c9b7238d850a62063afc1659ef58
jonathantang <jonathantang0126@gmail.com>
2019-09-21 10:06:10 +0800

    rewrite seeder.js

--- models/seeds/seeder.js
+++ models/seeds/seeder.js
@@ -1,21 +1,51 @@
 const mongoose = require('mongoose')
-const Record = require('../record')
+const bcrypt = require('bcryptjs')
 
-mongoose.connect('mongodb://localhost/expenseRecord', { useNewUrlParser: true, useUnifiedTopology: true })
+const Record = require('../record')
+const User = require('../user')
+
+const recordList = require('./Record.json')
+const userList = require('./User.json')
+
+mongoose.connect('mongodb://localhost/expenseRecord', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+
 const db = mongoose.connection
 
 db.on('error', () => {
-  console.log('mongodb error!')
+  console.log('db error')
 })
 
 db.once('open', () => {
-  for (var i = 0; i < 10; i++) {
-    Record.create({
-      name: 'name-' + i,
-      category: 'category' + i,
-      amount: i,
+  console.log('db connected!')
+  for (let i = 0; i < userList.results.length; i++) {
+    const newUser = new User({
+      name: userList.results[i].name,
+      email: userList.results[i].email,
+      password: userList.results[i].password
     })
-  }
 
-  console.log('done')
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) throw err
+        newUser.password = hash
+
+        newUser.save().then(users => {
+          for (var j = i * 5; j < (i * 5) + 5; j++) {
+            Record.create({
+              userId: users._id,
+              name: recordList.results[j].name,
+              category: recordList.results[j].category,
+              amount: recordList.results[j].amount,
+              date: recordList.results[j].date
+            })
+          }
+        }).catch(err => {
+          console.log(err)
+        })
+      })
+    })
+
+  }
+  console.log('Created seed!')
 })
+
